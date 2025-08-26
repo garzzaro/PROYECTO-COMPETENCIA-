632e014dc04b09381ea8cced5b37a27973bc29f9
maxzadi <maxzadi20@gmail.com>
2025-08-26 08:44:51 -0600

    creacion de usuario y atributos

---
+++ Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario{
+    private String nombre;
+    private String correo;
+    private String contra;
+    private String carné;
+    public ArrayLis<Calificacion> calificacion;
+
+    
+}
