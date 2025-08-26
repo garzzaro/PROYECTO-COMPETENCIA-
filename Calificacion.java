539891302c2f0922fc5a7f766e12876cd2aecc0e
ErickGarcia130705 <chinitofer8@outlook.com>
2025-08-26 09:13:52 -0600

    Creé Calificacion

---
+++ Calificacion.java
@@ -0,0 +1,14 @@
+public class Calificacion{
+
+    private Usuario persona;
+    private Reto reto;
+    private int puntuacionFinal;
+
+
+    public int sumaCalificacion(){
+    }
+
+    public int getCalificacion(){
+    return puntuacionFinal;
+    }
+}
