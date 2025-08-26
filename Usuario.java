41e377110bf2e876d18850d0988cb20ccf8e7e3f
maxzadi <maxzadi20@gmail.com>
2025-08-26 08:47:57 -0600

    creacion de constructor y metodos de Usuario

--- Usuario.java
+++ Usuario.java
@@ -1,9 +1,17 @@
-public class Usuario{
-    private String nombre;
-    private String correo;
-    private String contra;
-    private String carné;
-    public ArrayLis<Calificacion> calificacion;
+import java.util.ArrayList;
 
+public class Usuario{
+    private String nombre,
+    private String correo,
+    private String contra,
+    private String carné,
+    public ArrayLis<Calificacion> calificacion;
     
+    public Usuario(String nombre, String correo, String contra, String carné){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contra = contra;
+        this.carné = carné;
+        calificacion = new ArrayList<Calificacion>()
+    }
 }
