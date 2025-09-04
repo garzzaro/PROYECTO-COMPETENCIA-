99c91d846bc011fbcc604f8c97523d5480c26c8b
garzzaro <barbaragarzaro06@gmail.com>
2025-09-03 20:17:48 -0600

    Cambio1

--- Modelo/Usuario.java
+++ Modelo/Usuario.java
@@ -1,39 +1,35 @@
 package Modelo;
 import java.util.ArrayList;
 
-public class Usuario{
-    private String nombre,
-    private String correo,
-    private String contra,
-    private String carné,
-    public ArrayLis<Calificacion> calificacion;
-    
-    public Usuario(String nombre, String correo, String contra, String carné){
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contra;
+    private String carne;
+    private ArrayList<Calificacion> calificacion;
+
+    public Usuario(String nombre, String correo, String contra, String carne) {
         this.nombre = nombre;
         this.correo = correo;
         this.contra = contra;
-        this.carné = carné;
-        calificacion = new ArrayList<Calificacion>()
+        this.carne = carne;
+        calificacion = new ArrayList<Calificacion>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Calificacion> getCalificacion() {
+        return calificacion;
+    }
+
+    public void addCalificacion(Calificacion valor) {
+        this.calificacion.add(valor);
+    }
+
+    public String crearGrupo() {
+        String nombreGrupo = "Grupo_" + this.nombre;
+        return nombreGrupo;
     }
 }
-public String getNombre(){
-    return Nombre;
-     }
-
-public int getCalificacion(){
-    return Calificacion;
-     }
-
-public void sumaCalificacion(int valor)
-{
-    this.calificacion += valor;
-     }
-
-    public void sumaCalificacion () {
-        this.calificacion += 1;
-    }
-    
-public String crearGrupo(){
-    String nombreGrupo= "Grupo_"+this.nombre;
-    return nombreGrupo; 
- }
