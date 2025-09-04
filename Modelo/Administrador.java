6aa938a45cd92aa6056e13eed8fcf4bef0471ee1
maxzadi <maxzadi20@gmail.com>
2025-09-03 18:11:41 -0600

    creacion carpeta modelos

---
+++ Modelo/Administrador.java
@@ -0,0 +1,20 @@
+package Modelo;
+public  class Administrador{
+
+//atributos
+
+    public boolean admin;
+
+//métodos
+
+    public void asignarUsuario(){
+        
+    }
+    public void desasignarUsuario(){
+
+    }
+    public void punteoRetos(){
+
+    }
+
+}
---
+++ Modelo/Calificacion.java
@@ -0,0 +1,15 @@
+package Modelo;
+public class Calificacion{
+
+    private Usuario persona;
+    private Reto reto;
+    private int puntuacionFinal;
+
+
+    public int sumaCalificacion(){
+    }
+
+    public int getCalificacion(){
+    return puntuacionFinal;
+    }
+}
